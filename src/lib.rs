40006df6787878c5a99c785fa6e0a789fb527f35
Admin <mrAnton911@gmail.com>
2016-02-02 22:56:22 +0200

    Upload

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+//! Task queue
+//!
+//! # Examples
+//! ```
+//! extern crate task_queue;
+//! use task_queue;
+//! use std::sync::{ Arc, Mutex };
+//!
+//! let data = Arc::new(Mutex::new(0));
+//! let mut queue = task_queue::TaskQueue::new();
+//!
+//! for _ in 0..1000 {
+//!    let clone = data.clone();
+//!
+//!    queue.enqueue(move || {
+//!        let mut guard = clone.lock().unwrap();
+//!        *guard += 1;
+//!    }).unwrap();
+//! }
+//!
+//! let not_executed_tasks = queue.stop().unwrap();
+//! for t in &not_executed_tasks {
+//!     t.run();
+//! }
+//! ```
+
+pub mod task_queue;
+
+#[cfg(test)]
+mod test
+{
+    use std::sync::mpsc;
+    use std::sync::{ Arc, Mutex };
+    use task_queue;
+
+    #[test]
+    fn test_work() {
+        let data = Arc::new(Mutex::new(0));
+        let (sender, reciver) = mpsc::channel::<i32>();
+
+        let mut queue = task_queue::TaskQueue::new();
+        for _ in 0..1000 {
+            let clone = data.clone();
+            let sender_clone = sender.clone();
+
+            queue.enqueue(move || {
+                let mut guard = clone.lock().unwrap();
+                sender_clone.send(*guard).unwrap();
+
+                *guard += 1;
+            }).unwrap();
+        }
+
+        for i in 0..1000 {
+            let value = reciver.recv().unwrap();
+            assert_eq!(i, value);
+        }
+    }
+
+    #[test]
+    fn test_stop() {
+        let data = Arc::new(Mutex::new(0));
+        let mut queue = task_queue::TaskQueue::new();
+
+        for _ in 0..1000 {
+            let clone = data.clone();
+
+            queue.enqueue(move || {
+                let mut guard = clone.lock().unwrap();
+                *guard += 1;
+            }).unwrap();
+        }
+
+        let not_executed_tasks = queue.stop().unwrap();
+        for t in &not_executed_tasks {
+            t.run();
+        }
+
+        let num = data.lock().unwrap();
+        assert_eq!(*num, 1000);
+    }
+}
---
+++ src/task_queue/error.rs
@@ -0,0 +1,42 @@
+use std::error::Error;
+use std::fmt:: { Display, Formatter };
+use std::fmt;
+use std::option::Option;
+use std::io;
+
+#[derive(Debug)]
+pub enum TaskQueueError {
+    Io(io::Error),
+    Join,
+}
+
+impl Error for TaskQueueError {
+    fn description(&self) -> &str {
+        match *self {
+            TaskQueueError::Io(ref e) => e.description(),
+            TaskQueueError::Join => "Error on JoinHandle::Error invoke",
+        }
+    }
+
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            TaskQueueError::Io(ref e) => Some(e),
+            TaskQueueError::Join => None,
+        }
+    }
+}
+
+impl Display for TaskQueueError {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match *self {
+            TaskQueueError::Io(ref err) => write!(f, "IO error: {}", err),
+            TaskQueueError::Join => write!(f, "Join error: {}", self.description()),
+        }
+    }
+}
+
+impl From<io::Error> for TaskQueueError {
+    fn from(err: io::Error) -> TaskQueueError {
+        TaskQueueError::Io(err)
+    }
+}
